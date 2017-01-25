9b9716e184de02f6f822323b7e44bc1bef35b675
alexPi <iceman.delgado@gmail.com>
2017-01-26 00:01:06 +0100

    changes

--- app/index.js
+++ app/index.js
@@ -3,7 +3,8 @@ let terminalName =  "terminalSonatoPi";
 let serverId =      "serverPi";
 
 var io = require('socket.io-client'),
-socket = io.connect('http://192.168.1.99:8080');
+socket = io.connect('http://192.168.1.97:8080');
+var wpi = require('wiring-pi');
 
 socket.on('connect', function () {
   console.log("socket connected");
@@ -36,6 +37,28 @@ socket.emit('messageFromTerminalToServer', terminalName, "connection");
 function abrirPuerta(){
   console.log("Terminal: recibido orden de abrir puerta");
   socket.emit('messageFromTerminalToServer', terminalName, "doorOpened");
+
+// GPIO pin of the led
+var configPin = 7;
+// Blinking interval in usec
+var configTimeout = 1000;
+
+wpi.setup('wpi');
+wpi.pinMode(configPin, wpi.OUTPUT);
+
+var isLedOn = 0;
+var i = 0;
+
+var interval = setInterval(function() {
+	i++;
+	if (i <= 6){
+	isLedOn = +!isLedOn;
+	//isLedOn = !isLedOn;
+	wpi.digitalWrite(configPin, isLedOn );
+	}else{
+	clearInterval(interval);
+	}
+}, configTimeout);
 
   return false;
 
